1c1894c76038fd104b19c6177915f0349f9a1dca
Anatoly Samultsov <asamults@noreply.learn.01founders.co>
2025-04-22 18:33:03 +0000

    Update printalphabet/main.go

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/01-edu/z01"
 )
 
 func main() {
 	for ch := 'a'; ch <= 'z'; ch++ {
-		fmt.Printf("%c", ch)
+		z01.PrintRune(ch)
 	}
-	fmt.Println('\n')
+	fmt.Println() // Corrected: Use fmt.Println to print a newline
 }
